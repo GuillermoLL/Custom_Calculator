dd0b8b2d4603d6b3adbfd34f44bdb2c26759dc9c
PatitaDelta <guilmonster@hotmail.com>
2024-09-30 10:08:02 +0000

    calculator form index

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { CalculatorComponent } from "./components/calculator/calculator.component";
-import { CommonModule } from '@angular/common';
-import { Calculator } from './components/calculator/calculator.type';
 import { BoardComponent } from './pages/board/board.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, BoardComponent],
+  imports: [RouterOutlet, BoardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/components/add-calculator-form/index.ts
@@ -0,0 +1,1 @@
+export * from './add-calculator-form.component';
